d2db6a0cdd13c456e52be7bd559815a1f3e3c6b8
Timmy Liaw <timmy1544@gmail.com>
2021-12-21 15:41:12 -0800

    try deploy

--- database/index.js
+++ database/index.js
@@ -1,4 +1,12 @@
-const { CONFIG } = require('../config.js')
+// const { CONFIG } = require('../config.js')
+const CONFIG = {
+  user:'postgres',
+  host:'catwalk-reviews.cpao02pqmy0s.us-west-1.rds.amazonaws.com',
+  database:'postgres',
+  password:'Rootpass',
+  port:5432
+}
+
 const Pool = require('pg').Pool;
 
 const pool = new Pool(CONFIG);
