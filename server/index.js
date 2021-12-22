9949d76c8e4864ee4c89774125f67a495e367165
Timmy Liaw <timmy1544@gmail.com>
2021-12-21 18:47:32 -0800

    verify for loader.io

--- server/index.js
+++ server/index.js
@@ -11,6 +11,11 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use('/api/reviews', router);
 
+// for loader.io testing
+app.use('/loaderio-3e06ec0bbf880911e0e77fc78bbe3073/', (req, res) => {
+  res.status(200).send('loaderio-3e06ec0bbf880911e0e77fc78bbe3073');
+})
+
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
